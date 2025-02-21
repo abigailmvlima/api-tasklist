0f21d2b861fb6191b5d259ac0d71a4c83b82ac67
Abigail Lima <abigailmvlima@outlook.com>
2025-02-21 11:02:13 -0300

    feat: first commit

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "102030",
+  database: "tasklist",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};

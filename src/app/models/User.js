86e3d034392265fd845b421fadab8a0407e13bf6
Abigail Lima <abigailmvlima@outlook.com>
2025-02-24 11:07:12 -0300

    feat: generating hash

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,7 @@
+import bcrypt from "bcryptjs";
 import { Model, Sequelize } from "sequelize";
+
+const bcrypt = require("bcryptjs");
 
 class User extends Model {
   static init(sequelize) {
@@ -6,12 +9,21 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
       },
       {
         sequelize,
+        tableName: "users",
+        timestamps: true,
       }
     );
+    this.addHook("beforeSave", async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+    return this;
   }
 }
 

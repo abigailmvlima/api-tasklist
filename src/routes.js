082fe5eb602d88ec60767439cf429d227d3339df
Abigail Lima <abigailmvlima@outlook.com>
2025-02-24 11:35:27 -0300

    feat: autentication JWT

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import SessionControler from "./app/controllers/SessionControler";
 import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
 routes.post("/users", UserController.store);
+
+routes.post("/sessions", SessionControler.store);
 
 export default routes;

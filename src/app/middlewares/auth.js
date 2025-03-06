ada454a340e02258e427508d9bbc9c5fdc013f96
Abigail Lima <abigailmvlima@outlook.com>
2025-03-06 10:01:53 -0300

    feat: validating fields

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -14,7 +14,7 @@ export default async (req, res, next) => {
     const decoded = jwt.verify(token, authConfig.secret);
     req.userId = decoded.id;
 
-    console.log(decoded);
+    req.userId = decoded.id;
     return next();
   } catch (err) {
     return res.status(401).json({ error: "Token invalido" });
